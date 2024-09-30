484d1fc51b473d0773fbef12e2caebc88ed948b2
ericroys-school <eric.roys.school@gmail.com>
2024-09-30 15:22:42 -0500

    init commit

---
+++ src/test/java/com/edr/com/edr/technews/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edr.com.edr.technews.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
